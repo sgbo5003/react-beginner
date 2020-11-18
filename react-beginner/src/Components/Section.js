78f80f88ed6bd60b112331abb2038638e557c5ca
sgbo5003 <sgbo5003@naver.com>
2020-11-19 00:01:18 +0900

    11/18 공부내용(~#6-2 TVPresenter and Loader Components)

---
+++ react-beginner/src/Components/Section.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+const Container = styled.div`
+  // 마지막 children에게 margin-bottom을 주고 싶지 않다.
+  :not(:last-child) {
+    margin-bottom: 50px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Grid = styled.div`
+  margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+  grid-gap: 25px;
+`;
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </Container>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Section;
